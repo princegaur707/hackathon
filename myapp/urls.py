fae9d0a3246e9fdc347587a2f18a2ae7db493112
abhisheksaxena1998 <abhi123923@gmail.com>
2020-01-10 11:37:05 +0530

    added files

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('getuserfeedbackform',views.getuserfeedbackform,name="getuserfeedbackform"),
     path('saveuserfeedbackform',views.saveuserfeedbackform,name="saveuserfeedbackform"),
+    path('result',views.result,name='result'),
+    path('about',views.about,name='about'),
 
 ]
